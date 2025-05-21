293ece417cfd4b23ea710c41e165f94ec81fbcfb
5p7Ro0t <spandanpattanayak234@gmail.com>
2025-05-21 18:08:20 +0530

    Remove unwanted method

--- user-service/src/main/java/com/riderspoint/user_service/service/interfaces/SocialService.java
+++ user-service/src/main/java/com/riderspoint/user_service/service/interfaces/SocialService.java
@@ -4,6 +4,5 @@ import com.riderspoint.user_service.DTO.SocialDetailsDto;
 
 public interface SocialService {
 	SocialDetailsDto getSocialDetails(String username);
-	void addSocialDetails(String username, SocialDetailsDto socialDetails);
 	void updateSocialDetails(String username, SocialDetailsDto socialDetails);
 }
