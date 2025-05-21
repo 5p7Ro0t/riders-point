9af00414498e45dd9b09cf06bd91545df18c3045
5p7Ro0t <spandanpattanayak234@gmail.com>
2025-05-21 14:53:58 +0530

    Implement updateSocialDetails Method

--- user-service/src/main/java/com/riderspoint/user_service/service/impl/SocialServiceImpl.java
+++ user-service/src/main/java/com/riderspoint/user_service/service/impl/SocialServiceImpl.java
@@ -4,16 +4,22 @@ import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import com.riderspoint.user_service.DTO.SocialDetailsDto;
-import com.riderspoint.user_service.repository.SocialDetailsRepository;
+import com.riderspoint.user_service.entity.SocialDetailsEntity;
+import com.riderspoint.user_service.entity.UserProfileEntity;
+import com.riderspoint.user_service.repository.UserProfileRepository;
 import com.riderspoint.user_service.service.interfaces.SocialService;
 
 @Service
 public class SocialServiceImpl implements SocialService {
 
-	private final SocialDetailsRepository socialRepo;
+	private final UserProfileRepository userRepo;
 	private final ModelMapper modelMapper;
-	public SocialServiceImpl(SocialDetailsRepository socialRepo, ModelMapper modelMapper) {
-		this.socialRepo = socialRepo;
+	
+	
+
+	public SocialServiceImpl(UserProfileRepository userRepo, ModelMapper modelMapper) {
+		super();
+		this.userRepo = userRepo;
 		this.modelMapper = modelMapper;
 	}
 
@@ -24,15 +30,27 @@ public class SocialServiceImpl implements SocialService {
 	}
 
 	@Override
-	public void addSocialDetails(String username, SocialDetailsDto socialDetails) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
 	public void updateSocialDetails(String username, SocialDetailsDto socialDetails) {
 		// TODO Auto-generated method stub
-
+		UserProfileEntity user = userRepo.findByUsername(username)
+				.orElseThrow(() ->  new RuntimeException("User Not Found"));
+		if(user.getSocialDetails() == null) {
+			SocialDetailsEntity socialDetailsEntity = modelMapper.map(socialDetails, SocialDetailsEntity.class);
+			socialDetailsEntity.setUserProfile(user);
+			user.setSocialDetails(socialDetailsEntity);
+		}
+		else {
+			SocialDetailsEntity socialDetailsEntity = user.getSocialDetails();
+			if(socialDetails.getInstagram_url() != null) {
+				socialDetailsEntity.setInstagram_url(socialDetails.getInstagram_url());
+			}
+			if(socialDetails.getLinkedin_url() != null) {
+				socialDetailsEntity.setLinkedin_url(socialDetails.getLinkedin_url());
+			}
+			if(socialDetails.getYoutube_url() != null) {
+				socialDetailsEntity.setYoutube_url(socialDetails.getYoutube_url());
+			}
+		}
+		userRepo.save(user);
 	}
-
 }
