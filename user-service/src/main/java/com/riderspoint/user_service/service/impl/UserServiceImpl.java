a4f9ef7fd3e0d26954ae553926367929b1dc1ba7
5p7Ro0t <spandanpattanayak234@gmail.com>
2025-06-24 00:45:22 +0530

    Inject JWTService Dependency

--- user-service/src/main/java/com/riderspoint/user_service/service/impl/UserServiceImpl.java
+++ user-service/src/main/java/com/riderspoint/user_service/service/impl/UserServiceImpl.java
@@ -9,21 +9,26 @@ import com.riderspoint.user_service.DTO.UserProfileDto;
 import com.riderspoint.user_service.entity.UserProfileEntity;
 import com.riderspoint.user_service.repository.UserProfileRepository;
 import com.riderspoint.user_service.service.interfaces.UserService;
+import com.riderspoint.user_service.utility.JwtService;
 
 @Service
 public class UserServiceImpl implements UserService {
 
 	private final UserProfileRepository userRepo; 
 	private ModelMapper modelMapper;
+	private JwtService jwtService;
 	
-	public UserServiceImpl(UserProfileRepository userRepo, ModelMapper modelMapper) {
+	public UserServiceImpl(UserProfileRepository userRepo, ModelMapper modelMapper, JwtService jwtService) {
 		this.userRepo = userRepo;
 		this.modelMapper = modelMapper;
+		this.jwtService = jwtService;
 	}
 	
 	@Override
-	public UserProfileDto getUserProfile(String username) {
+	public UserProfileDto getUserProfile(String authToken) {
 		// TODO Auto-generated method stub
+		
+		String username = jwtService.extractSubject(authToken);
 		
 		Optional<UserProfileEntity> userOpt = userRepo.findByUsername(username);
 		
