5f2a73145744d92ccaf57a7eccc5af420e1147cb
5p7Ro0t <spandanpattanayak234@gmail.com>
2025-06-24 00:44:14 +0530

    Update Parameter

--- user-service/src/main/java/com/riderspoint/user_service/service/interfaces/UserService.java
+++ user-service/src/main/java/com/riderspoint/user_service/service/interfaces/UserService.java
@@ -3,7 +3,7 @@ package com.riderspoint.user_service.service.interfaces;
 import com.riderspoint.user_service.DTO.UserProfileDto;
 
 public interface UserService {
-	UserProfileDto getUserProfile(String username);
-	void createUserProfile(String username, UserProfileDto profile);
-    void updateBasicProfile(String username, UserProfileDto profile);
+	UserProfileDto getUserProfile(String authToken);
+	void createUserProfile(String authToken, UserProfileDto profile);
+    void updateBasicProfile(String authToken, UserProfileDto profile);
 }
